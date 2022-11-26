7e6d5eac22031d0d6b29f795ee27462cbacccb51
Dikshak Poudel <dikshak@truemark.com.np>
2022-11-26 12:26:55 +0545

    complete login functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = new express.Router();
 
-const { registerUser } = require("../controller/userController");
+const { registerUser, loginUser } = require("../controller/userController");
 
 // Customer Register
 router.post("/register", registerUser);
+
+// Customer Login
+router.post("/login", loginUser);
 
 module.exports = router;
